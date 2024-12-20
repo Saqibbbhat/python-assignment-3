39cb41a3d56450e66ebe4ff4b588f510f6a99c67
Saqibbbhat <saqibmanzoorbhat333@gmail.com>
2024-12-20 19:31:46 +0530

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,14 @@
+import math
+import random
+
+# Using math module
+def rhombus_area(diagonal1, diagonal2):
+    return (diagonal1 * diagonal2) / 2
+
+# Using random module
+def generate_random_numbers(n, a, b):
+    return [random.randint(a, b) for _ in range(n)]
+
+if _name_ == "_main_":
+    print(rhombus_area(10, 8))  # Output: 40.0
+    print(generate_random_numbers(5, 1, 100))
